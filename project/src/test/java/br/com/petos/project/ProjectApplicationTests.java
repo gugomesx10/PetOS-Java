8ddde10ab860392b81f40f5775550354f8f84593
gugomesx10 <gustavogomesmartins4@icloud.com>
2026-08-18 10:40:08 -0300

    test: adiconei também contexto da aplicação em teste

---
+++ project/src/test/java/br/com/petos/project/ProjectApplicationTests.java
@@ -0,0 +1,23 @@
+package br.com.petos.project;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    @DisplayName("O contexto da aplicacao PetOS deve carregar com sucesso")
+    void contextLoads() {
+        assertThat(applicationContext).isNotNull();
+    }
+}
+
